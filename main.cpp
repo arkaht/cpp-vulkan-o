04d03dde2e2aa2ae6b3820e5a8b18dd1cbed6e0a
Arthur Cathelain <114919245+arkaht@users.noreply.github.com>
2023-10-09 10:28:21 +0200

    [init] projekt

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#define GLFW_INCLUDE_VULKAN
+
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+
+#include <glm/glm.hpp>
+#include <glm/mat4x4.hpp>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+int main() {
+	glfwInit();
+	glfwWindowHint( GLFW_CLIENT_API, GLFW_NO_API );
+
+	GLFWwindow* window = glfwCreateWindow( 800, 600, "Test", nullptr, nullptr );
+	uint32_t extensionCount = 0;
+	vkEnumerateInstanceExtensionProperties( nullptr, &extensionCount, nullptr );
+	cout << "Extension count: " << extensionCount << endl;
+
+	while ( !glfwWindowShouldClose( window ) ) {
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow( window );
+	glfwTerminate();
+	return 0;
+}
